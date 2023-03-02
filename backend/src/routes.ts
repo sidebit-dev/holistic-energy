1f24eb29691bd672c7dbae26603a7fe90d2db63e
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-03-02 00:47:12 -0300

    Error and cors

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,9 +3,10 @@ import { Router, Request, Response } from "express";
 const router = Router()
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({
-        Ok: true
-    })
+   return res.json({Sistema: "Holistic Ernergy - Online!!"})
+
+   // Teste de Erro
+   // throw new Error('Erro ao fazer essa requisição')
 })
 
 export { router }
