0ebdb81c5f88d2ab2e57a36e3765686237a6ca0b
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-03-07 23:45:59 -0300

    Login user and gerated token JWT

--- backend/src/services/user/AuhtUserService.ts
+++ backend/src/services/user/AuhtUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken'
 
 interface AuthRequest{
     email: string;
@@ -27,9 +28,27 @@ class AuthUserService{
             throw new Error("Usuário ou senha incorreto");
         }
 
-        
+        // Gerar um token e devolver os dados do usuário com id, name, email e role
+        // Se deu tudo certo, vamos gerar o token pro usuário
+        const token = sign(
+            {
+            name: user.name,
+            email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+               subject: user.id,                             
+               expiresIn: '30d'               
+            }
+            )       
 
-        return { Ok: true}
+        return { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: token
+        }
     }
 }
 
