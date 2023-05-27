0ee289b82b4ec16048a3ed225777c5c8d72c9401
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-05-27 20:57:29 -0300

    Teste do Tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
+    <main className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-green-500 to-blue-500`}>
       <h1>Home</h1>
     </main>
   )
